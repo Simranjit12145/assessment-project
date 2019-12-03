a6d21490eb2f9685aa20a8de8d06b5a67763bfd0
Simranjit12145 <44260636+Simranjit12145@users.noreply.github.com>
2019-12-03 21:28:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function getRecipes(query) {
     $.ajax({
         url: `${host}/search?app_id=${appId}&app_key=${apiKey}&q=${query}`,
         success: function (result) {
-            // perloadfunction();
+           // perloadfunction();
             console.log(result);
             index = 0;
             data = result;
@@ -18,11 +18,12 @@ function getRecipes(query) {
     });
 }
 
-// function perloadfunction() {
-//     var load = document.getElementById('loading');
-//     loading.style.display = 'none';
-
-// }
+function cleandata()
+{
+    document.getElementById("recipes").innerHTML="";
+    document.getElementById("recipes").style="";
+    document.getElementById("search-input").value="";
+}
 
 function kFormatter(num) {
     return Math.abs(num) > 999 ?
@@ -53,88 +54,6 @@ function loadRecipes(result) {
     });
 }
 
-// Nutrition Analysis
-function getNutrientsAnalysis(data) {
-    $.ajax({
-        type: "POST",
-        url: `https://api.edamam.com/api/nutrition-details?app_id=47379841&app_key=d28718060b8adfd39783ead254df7f92`, //`${host}/api/nutrition-details?app_id=${appId}&app_key=${apiKey}`,
-        data: JSON.stringify(data),
-        dataType: "json",
-        contentType: "application/json",
-        success: function (result) {
-            console.log(result);
-            loadAnalysis(result);
-        }
-    });
-}
-function cleandata()
-{
-    document.getElementById("recipes").innerHTML="";
-    document.getElementById("recipes").style="";
-    document.getElementById("search-input").value="";
-
-}
-function loadAnalysis(result) {
-    $("#nutri-analysis").empty();
-    let rowData = "";
-    let ingredients = result.ingredients;
-    ingredients.forEach(ingredient => {
-        let parsedData = ingredient.parsed[0];
-        rowData += `
-<tr>
-  <th scope="row">${parsedData.quantity}</th>
-  <td>${parsedData.measure}</td>
-  <td>${parsedData.food}</td>
-  <td>${parsedData.weight}</td>
-</tr>
-`;
-    });
-
-    $("#nutri-analysis").append(`
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">Qty</th>
-      <th scope="col">Unit</th>
-      <th scope="col">Food</th>
-      <th scope="col">Weight</th>
-    </tr>
-  </thead>
-  <tbody>
-    ${rowData}
-  </tbody>
-</table>
-`);
-    $("#nutri-analysis").show();
-}
-
-// Pics
-function getPics(query) {
-    $.ajax({
-        url: `https://pixabay.com/api/?key=14364869-414b6dacd3d280678663edb95&q=${query}`,
-        success: function (result) {
-            loadPics(result);
-        }
-    });
-}
-
-function loadPics(result) {
-    let pics = result.hits;
-    pics.forEach(pic => {
-        $("#pics").empty();
-        $("#pics").append(
-            `<div class="pic-card card" style="width: 18rem;">
-  <img src="${pic.previewURL}" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">${pic.user}</h5>
-    <p class="card-text">Likes: ${pic.likes} | Views: ${pic.views}</p>
-  </div>
-</div>`
-        );
-        $("#pics").show();
-    });
-}
-
 $(document).ready(function () {
     // Get Recipes
     $("#search-button").click(function () {
@@ -152,37 +71,7 @@ $(document).ready(function () {
             }
         }
     });
-
-    // Get Nutrient Analysis
-    $("#analyse").click(function () {
-        let data = $("#ingredients-input").val();
-        console.log(data);
-        if (data) {
-            data = {
-                ingr: data.split(",")
-            };
-            console.log(data);
-            getNutrientsAnalysis(data);
-        }
-    });
-
-    // Get Nutrient Analysis
-    $("#pic-search-button").click(function () {
-        let userQuery = $("#pic-search-input").val();
-        if (userQuery) {
-            getPics(userQuery);
-        }
-    });
-    $("#pic-search-input").keypress(function (event) {
-        let key = event.key ? event.key : event.which;
-        if (key === "Enter") {
-            let userQuery = $("#pic-search-input").val();
-            if (userQuery) {
-                getPics(userQuery);
-            }
-        }
-    });
-});
+ });
 
 function openModalforIngredients(ind) {
     document.getElementById("datare").innerHTML = "";
@@ -210,21 +99,4 @@ function openModalforNutrient(ind) {
     }
     console.log(div1)
     document.getElementById("datare").append(div1);
-}
-
-
-//pop form 
-function formclose()
-{
-document.getElementById('id01').style.display='none';
-}
-
-
-function openForm() {
-    document.getElementById('id01').style.display = "block";
-  }
-
-  function myfunction(){
-     const name = $('#nameid').val();
-      $('.logo').attr('src',`https://joeschmoe.io/api/v1/${name}`);
 }
